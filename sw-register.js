5177c5170f94710e7b283176f106b082733e3b1f
king567-sudo <kingmba567@gmail.com>
2026-09-03 01:23:29 +0100

    Register main service worker alongside Firebase SW

--- sw-register.js
+++ sw-register.js
@@ -1,4 +1,10 @@
 if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js').then((registration) => {
+    console.log('Main Service Worker registered');
+  }).catch((err) => {
+    console.log('Main SW registration failed:', err);
+  });
+
   navigator.serviceWorker.register('firebase-messaging-sw.js').then(() => {
     console.log('Firebase Messaging Service Worker registered');
   }).catch((err) => {
